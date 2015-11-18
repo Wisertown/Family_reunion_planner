716375527b5cc03c1510909c730e15f6650225fd
Wisertown <aaron.wise253@gmail.com>
2015-11-18 15:54:25 -0500

    commit before the big push using codeigniter email function

--- application/views/admindash.php
+++ application/views/admindash.php
@@ -7,57 +7,67 @@
 	<title>Admin Page Smith Family Reunion</title>
 </head>
 <body>
-	<h4>Register A new User</h4>
-	<form action="Users/register" method="post">
-		<p>Name: <input type="text" name="name"></p>
-		<p>Username: <input type="text" name="username"></p>
-		<p>Password: <input type="password" name="password"></p>
-			<h5>*Password should be at least 8 characters</h5>
-		<p>Confirm Password: <input type="password" name="pw_confirm"></p>
-		<input class="btn" type="submit" value="Register">
-	</form>
-	<div>
-		<?= $this->session->flashdata("success_message"); ?>
+	<div class="nav">
+		<h4><a href="/adminlogin">users</a></h4>
+		<h4><a href="/dashboard">reunion locations</a></h4>
+		<h4><a href="/forum">posts/messages</a></h4>
 	</div>
-	<div id="errors">
-		<?= $this->session->flashdata("errors"); ?>
+	<div class="users">
+		<h2>Register A new User</h2>
+		<form action="Users/register" method="post" class="user_table">
+			<p>Name: <input type="text" name="name"></p>
+			<p>Username: <input type="text" name="username"></p>
+			<p>Password: <input type="password" name="password"></p>
+				<h5>*Password should be at least 8 characters</h5>
+			<p>Confirm Password: <input type="password" name="pw_confirm"></p>
+			<input class="btn" type="submit" value="Register">
+		</form>
+		<div>
+			<?= $this->session->flashdata("success_message"); ?>
+		</div>
+		<div id="errors">
+			<?= $this->session->flashdata("errors"); ?>
+		</div>
 	</div>
 	<br>
-	<h3>All Current Users</h3>
-	<table id="userview_edit">
-		<tr>
-			<th>id</th>
-			<th>name</th>
-			<th>username</th>
-			<th>password</th>
-			<th>created at</th>
-			<th>updated at</th>
-			<th>votes</th>
-			<th>switch status</th>
-			<th>created - by (admin)</th>
-			<th>edit</th>
-			<th>Delete</th>
-		</tr>
-		<?php foreach($useredits as $user_e) { ?>
-		<tr>
-			<td><?= $user_e['id'] ?></td>
-			<td><?= $user_e['name'] ?></td>
-			<td><?= $user_e['username'] ?></td>
-			<td><?= $user_e['password'] ?></td>
-			<td><?= $user_e['created_at'] ?></td>
-			<td><?= $user_e['updated_at'] ?></td>
-			<td><?= $user_e['votes'] ?></td>
-			<td><?= $user_e['switch'] ?></td>
-			<td><?= $user_e['admin'] ?></td>
-			<td><a href="/edit/<?= $user_e['id'] ?>">Edit this person</a></td>
-			<td>
-				<form action="/delete/<?= $user_e['id'] ?>" method="post">
-					<input type="submit" class="button" value="DELETE">
-				</form>
-			</td>
-		</tr>
-		<?php } ?>	
-	</table>
+		<h2 class="users">All Current Users</h2>
+	<div class="table_all">	
+		<table id="userview_edit">
+			<tr>
+				<th>id</th>
+				<th>name</th>
+				<th>username</th>
+				<th>password</th>
+				<th>created at</th>
+				<th>updated at</th>
+				<th>votes</th>
+				<th>switch status</th>
+				<th>created - by (admin)</th>
+				<th>edit</th>
+				<th>Delete</th>
+			</tr>
+			<?php foreach($useredits as $user_e) { ?>
+			<tr>
+				<td><?= $user_e['id'] ?></td>
+				<td><?= $user_e['name'] ?></td>
+				<td><?= $user_e['username'] ?></td>
+				<td><?= $user_e['password'] ?></td>
+				<td><?= $user_e['created_at'] ?></td>
+				<td><?= $user_e['updated_at'] ?></td>
+				<td><?= $user_e['votes'] ?></td>
+				<td><?= $user_e['switch'] ?></td>
+				<td><?= $user_e['admin'] ?></td>
+				<td><a href="/edit/<?= $user_e['id'] ?>">Edit this person</a></td>
+				<td>
+					<form action="/delete/<?= $user_e['id'] ?>" method="post">
+						<input type="submit" class="button" value="DELETE">
+					</form>
+				</td>
+			</tr>
+			<?php } ?>	
+		</table>
+	</div>
+
 	<a href="/Trips/logout_user"><p>Logout</p></a>
 </body>
 </html>
